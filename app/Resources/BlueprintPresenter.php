c9a49fc42933a10bca6cbea17fc6f7596cb0fbe6
Luuk Holleman <luukholleman@gmail.com>
2016-04-18 19:01:10 +0200

    Defined stats for blueprints to show on the detail page

--- app/Resources/BlueprintPresenter.php
+++ app/Resources/BlueprintPresenter.php
@@ -9,6 +9,46 @@ class BlueprintPresenter extends Presenter
 {
     public function imageUrl()
     {
+    }
 
+    public function getStatGroups()
+    {
+        return [
+            "Ratings" => [
+                "RatingExcitement",
+                "RatingIntensity",
+                "RatingNausea"
+            ],
+            "Drops" => [
+                "AirTime",
+                "Drops",
+                "TotalDropHeight",
+                "BiggestDrop"
+            ],
+            "Speeds" => [
+                "AverageSpeed",
+                "MaxSpeed"
+            ],
+            "G-Force" => [
+                "MinVertG",
+                "MaxVertG",
+                "MinLatG",
+                "MaxLatG",
+                "MinLongG",
+                "MaxLongG"
+            ],
+            "Ride" => [
+                "RideLengthDistance",
+                "RideLengthTime",
+                "Inversions",
+                "HeadChoppers"
+            ],
+            "Miscellaneous" => [
+                "EntranceFee",
+                "TrainCount",
+                "TrainLength",
+                "TrainTotal"
+            ]
+        ];
     }
 }
