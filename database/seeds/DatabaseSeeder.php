4ccc597d254ac1556b728b6fe2b6377a5a06c526
Luuk Holleman <luukholleman@gmail.com>
2016-04-25 19:27:14 +0200

    Added separate production seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminUserSeeder::class);
-        $this->call(TagSeeder::class);
-        $this->call(StatSeeder::class);
+        $this->call(ProductionSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(AssetSeeder::class);
         $this->call(AssetDependenciesSeeder::class);
---
+++ database/seeds/ProductionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class ProductionSeeder extends BaseSeeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(AdminUserSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(StatSeeder::class);
+    }
+}
