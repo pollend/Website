12aa82eed33da22604dafae229929ebb7c73d889
Luuk Holleman <luukholleman@gmail.com>
2016-05-01 22:10:55 +0200

    Try catch around like seeder because it can be unique in database

--- database/seeds/Development/LikeSeeder.php
+++ database/seeds/Development/LikeSeeder.php
@@ -14,14 +14,18 @@ class LikeSeeder extends BaseSeeder
         $likes = $assetCount * 25;
 
         for ($i = 0; $i < $likes; $i++) {
-            $asset = $this->getRandom(\PN\Assets\Asset::class);
-            $user = $this->getRandom(\PN\Users\User::class);
+            try {
+                $asset = $this->getRandom(\PN\Assets\Asset::class);
+                $user = $this->getRandom(\PN\Users\User::class);
 
-            factory(\PN\Social\Like::class)->create([
-                'user_id'       => $user->id,
-                'likeable_id'   => $asset->id,
-                'likeable_type' => get_class($asset),
-            ]);
+                factory(\PN\Social\Like::class)->create([
+                    'user_id'       => $user->id,
+                    'likeable_id'   => $asset->id,
+                    'likeable_type' => get_class($asset),
+                ]);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
         }
     }
 }
