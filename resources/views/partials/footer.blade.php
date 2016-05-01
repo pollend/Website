eecc34bc5c48c5f306d8d1b2514d033528e46c08
Tim Potze <timpotze@gmail.com>
2016-05-01 22:28:15 +0200

    Added footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<div class="container" id="footer">
+    <footer class="row">
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-xs-6">
+                    <a href="{{ route('pages.show', ['privacy']) }}" title="Privacy">
+                        Privacy
+                    </a>
+                    <p>Copyright © {{ date('Y') }} ParkitectNexus, Luuk Holleman
+                    </p>
+                </div>
+                <div class="col-xs-6 text-right">
+                    <table id="footer-contact">
+                        <tbody><tr>
+                            <td>
+                                <a href="https://twitter.com/parkitectnexus/" target="_blank" title="ParkitectNexus on Twitter">
+                                    ParkitectNexus on Twitter
+                                </a>
+                            </td>
+                            <td>
+                                <i class="fa fa-twitter"></i>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="mailto:info@parkitectnexus.com" title="Mail to ParkitectNexus">
+                                    info@parkitectnexus.com
+                                </a>
+                            </td>
+                            <td>
+                                <i class="fa fa-envelope"></i>
+                            </td>
+                        </tr>
+                        </tbody></table>
+                    <br>
+                    <br>
+                </div>
+            </div>
+            <br>
+        </div>
+    </footer>
+</div>
