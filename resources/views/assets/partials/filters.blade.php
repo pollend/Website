d29dc52931809485b8aaef3581efa7d3faebf6f3
Tim Potze <timpotze@gmail.com>
2016-05-02 21:06:37 +0200

    Put filters in panels

--- resources/views/assets/partials/filters.blade.php
+++ resources/views/assets/partials/filters.blade.php
@@ -2,22 +2,44 @@
     <form>
         <div class="form-group">
             Name
-            <input type="text" placeholder="Awesome Coaster" class="form-control" name="name" value="{{ request('name', '') }}">
+            <input type="text" placeholder="Awesome Coaster" class="form-control" name="name"
+                   value="{{ request('name', '') }}">
         </div>
 
-        <input type="hidden" name="sort" value="{{ request('sort') }}" />
-        <input type="hidden" name="range" value="{{ request('range') }}" />
+        <input type="hidden" name="sort" value="{{ request('sort') }}"/>
+        <input type="hidden" name="range" value="{{ request('range') }}"/>
 
-        <div id="sliders">
-            @foreach($filters['base'] as $name => $filter)
-                @include('assets.partials.filter', ['filter' => $filter])
-            @endforeach
-            @foreach($filters['advanced'] as $name => $filter)
-                @include('assets.partials.filter', ['filter' => $filter])
-            @endforeach
+        <div class="panel panel-default">
+            <div class="panel-heading"  data-toggle="collapse" data-target="#options-filter" aria-expanded="false" aria-controls="options-filter">
+                <h3 class="panel-title">
+                    <i class="glyphicon glyphicon-filter"></i>
+                    Filters
+                </h3>
+            </div>
+            <div class="panel-body{{ request('stats') == null ? ' collapse' : '' }}" id="options-filter">
+                <div id="sliders">
+                    @foreach($filters['base'] as $name => $filter)
+                        @include('assets.partials.filter', ['filter' => $filter])
+                    @endforeach
+                    @foreach($filters['advanced'] as $name => $filter)
+                        @include('assets.partials.filter', ['filter' => $filter])
+                    @endforeach
+                </div>
+            </div>
         </div>
-        @foreach($tags as $tag)
-            @include('assets.partials.tag-filter', ['tag' => $tag])
-        @endforeach
+
+        <div class="panel panel-default">
+            <div class="panel-heading"  data-toggle="collapse" data-target="#options-tags" aria-expanded="false" aria-controls="options-tags">
+                <h3 class="panel-title">
+                    <i class="glyphicon glyphicon-tag"></i>
+                    Tags
+                </h3>
+            </div>
+            <div class="panel-body{{ request('tags') == null ? ' collapse' : '' }}" id="options-tags">
+                @foreach($tags as $tag)
+                    @include('assets.partials.tag-filter', ['tag' => $tag])
+                @endforeach
+            </div>
+        </div>
     </form>
 </div>
