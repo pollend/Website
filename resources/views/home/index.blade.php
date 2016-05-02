acb2b627beed073fd7d330cea4b82a5b5f59719f
Tim Potze <timpotze@gmail.com>
2016-05-02 19:55:46 +0200

    Made homepage look better on mobile devices

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
     </h1>
 
     <div class="row">
-        <div class="col-sm-7">
+        <div class="col-md-7">
             <p>
                 Here you will find mods, coasters and parks created by players for Parkitect.
                 We already have <b>{{ $assetCount->getModCount() }} mods</b>,
@@ -17,67 +17,67 @@
                 It's even easier with the <a href="{{ route('pages.show', ['client']) }}">ParkitectNexus Client</a>
             </p>
         </div>
-        <div class="col-sm-5">
+        <div class="col-md-5 hidden-xs hidden-sm">
             <button href="{{ route('pages.show', ['client']) }}" class="btn btn-primary btn-lg btn-block">
                 Download the ParkitectNexus Client
             </button>
         </div>
     </div>
 
-    <hr />
+    <hr class="hidden-xs" />
 
-    <div class="row v-margin">
-        <div class="col-lg-4 border-right">
+    <div class="row v-margin home-asset-types hidden-xs">
+        <div class="col-md-4 border-right asset-type">
             <div class="row">
-                <div class="col-xs-5 text-center">
+                <a href="{{route('assets.filter', ['mod'])}}"class="col-xs-5 text-center symbol">
                     <i class="fa fa-wrench v-margin" style="font-size: 110px;"></i>
                     <h2>Mods</h2>
-                </div>
+                </a>
                 <div class="col-xs-7">
                     <p>
                         Want to go further than the game's limitations?
                         We have {{ $assetCount->getModCount() }} mods for countless hours of fun!
                     </p>
                     <p>
-                        <a class="btn btn-default v-margin" href="{{ route('assets.filter', ['mod']) }}" role="button" title="Mods">
-                            View all {{ $assetCount->getModCount() }} Mods »
+                        <a class="btn btn-default v-margin" href="{{ route('assets.filter', ['mod']) }}" role="button" title="Parks">
+                            View all »
                         </a>
                     </p>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 border-right">
+        <div class="col-md-4 border-right asset-type">
             <div class="row">
-                <div class="col-xs-5 text-center">
+                <a href="{{route('assets.filter', ['blueprint'])}}"class="col-xs-5 text-center symbol">
                     <i class="fa fa-flask v-margin" style="font-size: 110px;"></i>
                     <h2>Blueprints</h2>
-                </div>
+                </a>
                 <div class="col-xs-7">
                     <p>
                         If you're not that good at building things,
                         we have {{ $assetCount->getBlueprintCount() }} blueprints ready for download!
                     </p>
                     <p>
-                        <a class="btn btn-default v-margin" href="{{ route('assets.filter', ['blueprint']) }}" role="button" title="Blueprints">
-                            View all {{ $assetCount->getBlueprintCount() }} Blueprints »
+                        <a class="btn btn-default v-margin" href="{{ route('assets.filter', ['blueprint']) }}" role="button" title="Parks">
+                            View all »
                         </a>
                     </p>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-md-4 asset-type">
             <div class="row">
-                <div class="col-xs-5 text-center">
+                <a href="{{route('assets.filter', ['park'])}}"class="col-xs-5 text-center symbol">
                     <i class="fa fa-save v-margin" style="font-size: 110px;"></i>
                     <h2>Parks</h2>
-                </div>
+                </a>
                 <div class="col-xs-7">
                     <p>
                         Out of inspiration? We have {{ $assetCount->getParkCount() }} parks to refill your fantasy!
                     </p>
                     <p>
                         <a class="btn btn-default v-margin" href="{{ route('assets.filter', ['park']) }}" role="button" title="Parks">
-                            View all {{ $assetCount->getParkCount() }} Parks »
+                            View all »
                         </a>
                     </p>
                 </div>
@@ -93,7 +93,7 @@
 
     <div class="row" id="list">
         @foreach($popular as $asset)
-            <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="col-xs-6 col-md-3">
                 @include('assets.partials.thumbnail', ['asset' => $asset])
             </div>
         @endforeach
@@ -104,7 +104,7 @@
     </h2>
     <div class="row" id="list">
         @foreach($newest as $asset)
-            <div class="col-sm-12 col-md-6 col-lg-3">
+            <div class="col-xs-6 col-md-3">
                 @include('assets.partials.thumbnail', ['asset' => $asset])
             </div>
         @endforeach
