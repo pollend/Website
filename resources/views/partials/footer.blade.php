c73949af562eb24938267c296da4a0b3aa796f6b
Tim Potze <timpotze@gmail.com>
2016-05-03 18:54:16 +0200

    Fixed the footer being in a footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,42 +1,41 @@
-<div class="container" id="footer">
-    <footer class="row">
-        <div class="col-sm-12">
-            <div class="row">
-                <div class="col-xs-6">
-                    <a href="{{ route('pages.show', ['privacy']) }}" title="Privacy">
-                        Privacy
-                    </a>
-                    <p>Copyright © {{ date('Y') }} ParkitectNexus, Luuk Holleman
-                    </p>
-                </div>
-                <div class="col-xs-6 text-right">
-                    <table id="footer-contact">
-                        <tbody><tr>
-                            <td>
-                                <a href="https://twitter.com/parkitectnexus/" target="_blank" title="ParkitectNexus on Twitter">
-                                    ParkitectNexus on Twitter
-                                </a>
-                            </td>
-                            <td>
-                                <i class="fa fa-twitter"></i>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <a href="mailto:info@parkitectnexus.com" title="Mail to ParkitectNexus">
-                                    info@parkitectnexus.com
-                                </a>
-                            </td>
-                            <td>
-                                <i class="fa fa-envelope"></i>
-                            </td>
-                        </tr>
-                        </tbody></table>
-                    <br>
-                    <br>
-                </div>
+<footer class="row">
+    <div class="col-sm-12">
+        <div class="row">
+            <div class="col-xs-6">
+                <a href="{{ route('pages.show', ['privacy']) }}" title="Privacy">
+                    Privacy
+                </a>
+                <p>Copyright © {{ date('Y') }} ParkitectNexus, Luuk Holleman
+                </p>
             </div>
-            <br>
+            <div class="col-xs-6 text-right">
+                <table id="footer-contact">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <a href="https://twitter.com/parkitectnexus/" target="_blank"
+                               title="ParkitectNexus on Twitter">
+                                ParkitectNexus on Twitter
+                            </a>
+                        </td>
+                        <td>
+                            <i class="fa fa-twitter"></i>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="mailto:info@parkitectnexus.com" title="Mail to ParkitectNexus">
+                                info@parkitectnexus.com
+                            </a>
+                        </td>
+                        <td>
+                            <i class="fa fa-envelope"></i>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </footer>
-</div>
+        <br>
+    </div>
+</footer>
