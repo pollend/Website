3100ea5b68e0d12975092eaa3de7b6752f0c083d
Luuk Holleman <luukholleman@gmail.com>
2016-05-05 15:34:22 +0200

    Added trackers

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,5 +39,40 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.min.js"></script>
 <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
 @yield('js')
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//piwik.parkitectnexus.com/";
+        _paq.push(['setTrackerUrl', u + 'piwik.php']);
+        _paq.push(['setSiteId', 1]);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.type = 'text/javascript';
+        g.async = true;
+        g.defer = true;
+        g.src = u + 'piwik.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-67252152-1', 'auto');
+    ga('send', 'pageview');
+
+    ga('_setCustomVar', 1, 'logged_in', 'true', 2);
+</script>
+<noscript><p><img src="//piwik.parkitectnexus.com/piwik.php?idsite=1" style="border:0;" alt=""/></p></noscript>
 </body>
 </html>
