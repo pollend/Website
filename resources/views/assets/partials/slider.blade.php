bd25668e1e0a5387a9a4cc1efb8a4e5afbc1fe00
Luuk Holleman <luukholleman@gmail.com>
2016-05-16 12:55:55 +0200

    Fixed slider images (once again)

--- resources/views/assets/partials/slider.blade.php
+++ resources/views/assets/partials/slider.blade.php
@@ -1,17 +1,17 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-example-generic" class="carousel slide hidden-xs" data-ride="carousel">
     <div class="carousel-inner" role="listbox">
         <div class="item active">
             <img src="{{ $asset->getImage()->getPresenter()->source(1920, 1080) }}"/>
         </div>
         @foreach($asset->images as $image)
             <div class="item">
-                <img src="{{ $image->getPresenter()->source }}"/>
+                <img src="{{ $image->getPresenter()->source(1920, 1080) }}"/>
             </div>
         @endforeach
 
         @if($asset->type != 'mod')
             <div class="item">
-                <img src="{{ $asset->resource->getImage()->getPresenter()->source }}"/>
+                <img src="{{ $asset->resource->getImage()->getPresenter()->source(1920, 1080) }}"/>
             </div>
         @endif
     </div>
@@ -28,17 +28,17 @@
 
 <div class="slider-thumbnails">
     {{--<div class="col-sm-2">--}}
-        <img data-target="#carousel-example-generic" data-slide-to="0" width="100%" src="{{ $asset->getImage()->getPresenter()->source(116, 66) }}"/>
+        <img data-target="#carousel-example-generic" data-slide-to="0" width="100%" src="{{ $asset->getImage()->getPresenter()->source(1280, 720) }}"/>
     {{--</div>--}}
     @foreach($asset->images as $key => $image)
         {{--<div class="col-sm-2">--}}
-            <img data-target="#carousel-example-generic" data-slide-to="{{ $key + 1 }}" width="100%" src="{{ $image->getPresenter()->source(116, 66) }}"/>
+            <img data-target="#carousel-example-generic" data-slide-to="{{ $key + 1 }}" width="100%" src="{{ $image->getPresenter()->source(1280, 720) }}"/>
         {{--</div>--}}
     @endforeach
 
     @if($asset->type != 'mod')
         {{--<div class="col-sm-2">--}}
-            <img data-target="#carousel-example-generic" data-slide-to="{{ count($asset->images) + 1 }}" width="100%" src="{{ $asset->resource->getImage()->getPresenter()->source(116, 66) }}"/>
+            <img data-target="#carousel-example-generic" data-slide-to="{{ count($asset->images) + 1 }}" width="100%" src="{{ $asset->resource->getImage()->getPresenter()->source(1280, 720) }}"/>
         {{--</div>--}}
     @endif
 </div>
