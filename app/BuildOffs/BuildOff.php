2c41680738cec5c754548adf4d35bd7eeb417b77
Luuk Holleman <luukholleman@gmail.com>
2017-02-10 14:39:46 +0100

    Forgot another file!

--- app/BuildOffs/BuildOff.php
+++ app/BuildOffs/BuildOff.php
@@ -137,7 +137,7 @@ class BuildOff extends Model
         }
 
         if($asset->type == "blueprint") {
-            $price = $asset->getResource()->getExtractor()->getStats()["ApproximateCost"];
+            $price = $asset->getResource()->getStats()["ApproximateCost"];
 
             if($price > $this->max_price) {
                 return false;
